4f9e812654dcfda409e81f22a0cc25022c0cdeb5
MattGold65 <79160104+MattGold65@users.noreply.github.com>
2023-05-22 00:32:12 -0400

    Greatly Improved Efficiency of Program

--- flower.go
+++ flower.go
@@ -240,3 +240,23 @@ func InsertDataProductFlowerCommon(db *sql.DB, target FlowerData) {
 
 	}
 }
+
+func getFlowerData(db *sql.DB, dispensaryIDs []string, dispensaryTargets []FlowerData) {
+
+	for i, Dispensary := range dispensaryIDs {
+
+		DispensaryAPILink := "https://dutchie.com/graphql?operationName=FilteredProducts&variables=%7B%22includeEnterpriseSpecials%22%3Afalse%2C%22includeCannabinoids%22%3Atrue%2C%22productsFilter%22%3A%7B%22dispensaryId%22%3A%22" + Dispensary + "%22%2C%22pricingType%22%3A%22med%22%2C%22strainTypes%22%3A%5B%5D%2C%22subcategories%22%3A%5B%5D%2C%22Status%22%3A%22Active%22%2C%22types%22%3A%5B%22Flower%22%5D%2C%22useCache%22%3Afalse%2C%22sortDirection%22%3A1%2C%22sortBy%22%3Anull%2C%22isDefaultSort%22%3Atrue%2C%22bypassOnlineThresholds%22%3Afalse%2C%22isKioskMenu%22%3Afalse%2C%22removeProductsBelowOptionThresholds%22%3Atrue%7D%2C%22page%22%3A0%2C%22perPage%22%3A1000%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%220e884328c01ef8ed540d4bbd27101ee58fedaf5cddda6c499169209b53fdf574%22%7D%7D"
+
+		err := getJson(DispensaryAPILink, Dispensary+"FlowerData", &dispensaryTargets[i])
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	for _, FlowerMenu := range dispensaryTargets {
+		InsertDataProductFlowerCommon(db, FlowerMenu)
+
+	}
+
+}
